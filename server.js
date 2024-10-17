f13ae2530af07f4937451e6d63002db3ea7b01b5
SZ1l <hyrakle@gmail.com>
2024-10-17 23:16:19 +0500

    server.js

--- server.js
+++ server.js
@@ -42,6 +42,10 @@ async function startServer() {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
             }
         });
+         if (!response.ok) {
+            console.error(`Ошибка HTTP: ${response.status} при запросе ${productUrl}`);
+            return { kaspiPrice: null, productUrl };
+        }
         const text = await response.text();
         console.log(`Запрос по URL: ${productUrl}`);
 
